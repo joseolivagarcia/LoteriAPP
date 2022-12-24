281c506a4fc96faf90c331f32d7e93a4c4a13088
Jose Oliva Local <jose@joseoliva.com>
2022-12-24 18:42:27 +0100

    Preparada main activity con recyclerview e item de cada décimo con el que participamos

--- app/src/main/java/joseoliva/com/loteriapp/activities/MainActivity.kt
+++ app/src/main/java/joseoliva/com/loteriapp/activities/MainActivity.kt
@@ -3,10 +3,17 @@ package joseoliva.com.loteriapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import joseoliva.com.loteriapp.R
+import joseoliva.com.loteriapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //añadimos viewbinding para las vistas
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //inicializamos el binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
